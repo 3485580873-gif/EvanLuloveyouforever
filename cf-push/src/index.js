efd0902c2bb52231dc5982d4ad2d535586ab2b3d
3485580873-gif <3485580873@qq.com>
2026-07-23 12:56:23 +0800

    Update index.js

--- cf-push/src/index.js
+++ cf-push/src/index.js
@@ -11,7 +11,7 @@ function corsHeaders() {
   return {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type'
+    'Access-Control-Allow-Headers': 'Content-Type, X-App-Secret'
   };
 }
 
